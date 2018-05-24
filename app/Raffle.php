5fcdd8a441225a21eb276e4d1c4e309a4da27448
ramamoorthy <vik@appomate.com.au>
2018-05-24 16:12:32 +1000

    add fillable method(all fileds)

--- app/Raffle.php
+++ app/Raffle.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Raffle extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'no_of_prizes',
+        'total_value',
+        'organisation',
+        'last_selling_date',
+        'draw_date',
+        'draw_place',
+        'max_tickets',
+        'prize_per_ticket',
+        'discount_multiple',
+        'price',
+        'status'
+    ];
 }

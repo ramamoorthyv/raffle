d90870c05bcee39c1038de3312b7bf6c4ed94dc6
ramamoorthy <vik@appomate.com.au>
2018-05-24 11:14:02 +1000

    rename controller

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('raffle', 'RaffleController');
+Route::resource('raffles', 'RafflesController');
 Route::resource('prize', 'PrizeController');
 Route::resource('order', 'OrderController');
 Route::resource('organisation', 'OrganisationController');

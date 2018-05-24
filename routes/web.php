bd3259f49bd1cc82d666c2ce61d0a63b20735a08
ramamoorthy <vik@appomate.com.au>
2018-05-24 10:42:55 +1000

    add routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('raffle', 'RaffleController');
+Route::resource('prize', 'PrizeController');
+Route::resource('order', 'OrderController');
+Route::resource('organisation', 'OrganisationController');
+
